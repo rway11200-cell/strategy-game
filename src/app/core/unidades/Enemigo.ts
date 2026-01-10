a1044583770e3063749538360ba70245022c0a79
s0c0kww <sebastian.cornejo1@walmart.com>
2026-01-10 17:35:24 -0300

    Se anade esqueleto y fantasma

--- src/app/core/unidades/Enemigo.ts
+++ src/app/core/unidades/Enemigo.ts
@@ -5,9 +5,9 @@ export class Enemigo extends Unidad {
   constructor(contenedorPrincipal: Container, opciones?: UnidadProps) {
     if (opciones) {
       opciones.framesJson = {
-        idle: "goblin scout - silhouette all animations-idle.json",
-        run: "goblin scout - silhouette all animations-run.json",
-        dead: "goblin scout - silhouette all animations-death 1.json",
+        idle: "esqueleton-idle.json",
+        run: "esqueleton-run.json",
+        dead: "esqueleton-dead.json",
       };
     }
     super(contenedorPrincipal, opciones);
